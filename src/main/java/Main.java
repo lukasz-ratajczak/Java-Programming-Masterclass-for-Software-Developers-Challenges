477d6f8e05e3f38635c000af143460f53802eed0
Lukasz Ratajczak <miszeq12@gmail.com>
2019-06-06 21:06:21 +0200

    57. Min and Max Challenge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,19 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int con = 0;
-        int sum = 0;
-        while(con <10){
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        while (con != 1) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Give value: ");
+            System.out.println("Enter number: ");
             boolean isInt = scanner.hasNextInt();
-            if(isInt){
+            if (isInt) {
                 int value = scanner.nextInt();
-                sum +=value;
-                con++;
-            }else{
+
+                if (value > max) {
+                    max = value;
+                }
+                if (value < min) {
+                    min = value;
+                }
+
+            } else {
                 System.out.println("Invalid input");
+                con = 1;
             }
         }
-        System.out.println("Sum is: "+ sum);
+        System.out.println("Min is: " + min + " and Max is: " + max);
     }
 }
