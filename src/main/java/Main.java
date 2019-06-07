5e9f808aec3abea16932c560fa197ff7074c2e7b
Lukasz Ratajczak <miszeq12@gmail.com>
2019-06-07 13:17:50 +0200

    Exercise 29: Sum Calculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,8 @@
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
-        System.out.println(getBucketCount(3.4, 2.1, 1.5));
-        System.out.println(getBucketCount(3.4, 1.5));
-    }
 
-    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
-        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
-            return -1;
-        }
-        double area = width * height;
-        if (area < areaPerBucket * extraBuckets) {
-            return 0;
-        }
-        return (int) Math.ceil((area - areaPerBucket * extraBuckets) / areaPerBucket);
-    }
-
-    public static int getBucketCount(double width, double height, double areaPerBucket) {
-        if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
-            return -1;
-        }
-        double area = width * height;
-        return (int) Math.ceil(area / areaPerBucket);
-    }
-
-    public static int getBucketCount(double area, double areaPerBucket) {
-        if (area <= 0 || areaPerBucket <= 0) {
-            return -1;
-        }
-        return (int) Math.ceil(area / areaPerBucket);
     }
 }
