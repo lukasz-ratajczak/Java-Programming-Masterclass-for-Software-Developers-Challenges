b33816134acab11421dd94963d048a364f49d214
Lukasz Ratajczak <miszeq12@gmail.com>
2019-06-12 14:05:51 +0200

    63. Inheritance - Part 1

---
+++ src/main/java/Animal.java
@@ -0,0 +1,40 @@
+public class Animal {
+    private int brain, body, size, weight;
+    private String name;
+
+    public Animal(int brain, int body, int size, int weight, String name) {
+        this.brain = brain;
+        this.body = body;
+        this.size = size;
+        this.weight = weight;
+        this.name = name;
+    }
+
+    public void eat() {
+        System.out.println("Animal.eat call");
+    }
+
+    public void move() {
+
+    }
+
+    public int getBrain() {
+        return brain;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
