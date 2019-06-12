fbadae32af0c6d6d6c588680bc69c26e24d083cf
Lukasz Ratajczak <miszeq12@gmail.com>
2019-06-12 23:08:28 +0200

    Exercise 35: Cylinder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Animal animal = new Animal(1,1,5,5,"Animal");
-        Dog dog = new Dog(5, 5,"Yorky", 1,4,1,20,"long silky");
 
-        dog.eat();
-        dog.walk();
-        dog.run();
     }
 }
