1706bc9e3e5815d4f5919afaf2e78cfa26e58672
Lukasz Ratajczak <miszeq12@gmail.com>
2019-06-12 13:43:37 +0200

    34: Complex

---
+++ src/main/java/ComplexNumber.java
@@ -0,0 +1,38 @@
+public class ComplexNumber {
+    private double real, imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real = this.real + real;
+        this.imaginary = this.imaginary + imaginary;
+    }
+
+    public void add(ComplexNumber complexNumber) {
+        this.real = getReal() + complexNumber.getReal();
+        this.imaginary = getImaginary() + complexNumber.getImaginary();
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real = this.real - real;
+        this.imaginary = this.imaginary - imaginary;
+    }
+
+    public void subtract(ComplexNumber complexNumber) {
+        this.real = getReal() - complexNumber.getReal();
+        this.imaginary = getImaginary() - complexNumber.getImaginary();
+
+    }
+
+}
