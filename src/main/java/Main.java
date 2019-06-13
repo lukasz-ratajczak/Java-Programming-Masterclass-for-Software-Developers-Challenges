5297846bee27207668eeb95136a361916af132a5
Lukasz Ratajczak <miszeq12@gmail.com>
2019-06-13 17:43:49 +0200

    82. Arrays (Challenge Exercise)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,60 +1,53 @@
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Hamburger hamburger;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Define number of elements: ");
+        int numberOfElements = scanner.nextInt();
+        System.out.println("Define all elements: ");
+        int[] inputArray = getIntegers(numberOfElements);
+        System.out.println("Input:");
+        printArray(inputArray);
+
+        int[] outputArray = sortArray(inputArray);
+
+        System.out.println("Output:");
+        printArray(outputArray);
+    }
+
+    public static int[] sortArray(int[] inputArray){
+
+        int[] outputArray = new int[inputArray.length];
+        int[] tempArray = new int[inputArray.length];
+
+        for (int j = 0; j < inputArray.length; j++) {
+            for (int i = 0; i < inputArray.length-1; i++) {
+                if(inputArray[i] > inputArray[i+1]){
+                    tempArray[i] = inputArray[i+1];
+                    inputArray[i+1] = inputArray[i];
+                    inputArray[i] = tempArray[i];
+                }
+            }
+        }
+
+        return inputArray;
+    }
+
+    public static void printArray(int[] arrayToPrint){
+        for (int i1 : arrayToPrint) {
+            System.out.println(i1);
+        }
+    }
+    public static int[] getIntegers(int numberOfElements) {
         Scanner scanner = new Scanner(System.in);
 
-        int selector;
-
-        System.out.println("Hello. Make Your Burger by yourself! :)");
-
-        do {
-            boolean corrector;
-            System.out.println("First, choose a burger type:");
-            System.out.println("1) Hamburger");
-            System.out.println("2) Healthy burger");
-            System.out.println("3) Deluxe burger");
-            corrector = scanner.hasNextInt();
-            if (!corrector) {
-                System.out.println("Choose valid option");
-                continue;
-            }
-            selector = scanner.nextInt();
-            if (selector >= 1 && selector <= 3) {
-                hamburger = burgerTypeOption(selector);
-                break;
-            }
-            System.out.println("Choose valid option");
-        } while (true);
-
-
-        if (hamburger.getClass().getSimpleName().equals("Hamburger")) {
-            hamburger.getAdditions();
-            System.out.println("Your price for burger is: "+hamburger.getPrice()+"$");
-        } else if (hamburger.getClass().getSimpleName().equals("HealthyBurger")) {
-            hamburger.getAdditions();
-            System.out.println("Your price for burger is: "+hamburger.getPrice()+"$");
-        } else if (hamburger.getClass().getSimpleName().equals("DeluxeBurger")) {
-            hamburger.getAdditions();
-            System.out.println("Your price for burger is: "+hamburger.getPrice()+"$");
-        } else {
-            System.out.println("Error! :/");
+        int[] inputArray = new int[numberOfElements];
+        for (int i = 0; i < inputArray.length; i++) {
+            inputArray[i] = scanner.nextInt();
         }
-
-
+        return inputArray;
     }
 
-    public static Hamburger burgerTypeOption(int selector) {
-        switch (selector) {
-            case 1:
-                return new Hamburger();
-            case 2:
-                return new HealthyBurger();
-            case 3:
-                return new DeluxeBurger();
-            default:
-                return new Hamburger();
-        }
-    }
 }
