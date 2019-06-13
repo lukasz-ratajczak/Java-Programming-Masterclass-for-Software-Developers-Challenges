abc9642ba507f0ff5a664b70de1a87a972b486f6
Lukasz Ratajczak <miszeq12@gmail.com>
2019-06-13 11:18:43 +0200

    76. Encapsulation (+Challenge Exercise)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Paintings paintings = new Paintings(46, 499);
-        Electronic electronic = new Electronic(400, "TV");
-        Furniture furniture = new Furniture(400, "Chair");
+    Printer HP8020 = new Printer(60, 20, true);
+    Printer SM6040M = new Printer(50, 50, false);
 
-        Room room = new Room(4, furniture, electronic, paintings);
+        for (int i = 0; i < 30; i++) {
+            System.out.println("Printer HP:");
+            HP8020.printPage();
+            System.out.println(" ");
+            System.out.println("Printer SM:");
+            SM6040M.printPage();
+            System.out.println(" ");
+        }
+        HP8020.changeToner();
+        SM6040M.changeToner();
+        for (int i = 0; i < 30; i++) {
+            System.out.println("Printer HP:");
+            HP8020.printPage();
+            System.out.println(" ");
+            System.out.println("Printer SM:");
+            SM6040M.printPage();
+        }
+        System.out.println(" ");
+        System.out.println(HP8020.getTonerLevel());
+        System.out.println(SM6040M.getTonerLevel());
+        System.out.println(" ");
+        System.out.println(HP8020.getPrintedPages());
+        System.out.println(SM6040M.getPrintedPages());
 
-        House house = new House("Brick", "Plasteel", room);
-
-        house.makeRoom();
     }
 }
