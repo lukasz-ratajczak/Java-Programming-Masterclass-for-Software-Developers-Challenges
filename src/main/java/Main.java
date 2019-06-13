6ba9728bea4f3742c9b6d3aba3bcfa1b26c0fd01
Lukasz Ratajczak <miszeq12@gmail.com>
2019-06-13 12:25:02 +0200

    78. Polymorphism (+Challenge Exercise)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,18 @@
-import java.text.SimpleDateFormat;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-    Printer HP8020 = new Printer(60, 20, true);
-    Printer SM6040M = new Printer(50, 50, false);
+        Car skoda = new Skoda(16, "Fiesta");
+        skoda.accelerate(50);
 
-        for (int i = 0; i < 30; i++) {
-            System.out.println("Printer HP:");
-            HP8020.printPage();
-            System.out.println(" ");
-            System.out.println("Printer SM:");
-            SM6040M.printPage();
-            System.out.println(" ");
-        }
-        HP8020.changeToner();
-        SM6040M.changeToner();
-        for (int i = 0; i < 30; i++) {
-            System.out.println("Printer HP:");
-            HP8020.printPage();
-            System.out.println(" ");
-            System.out.println("Printer SM:");
-            SM6040M.printPage();
-        }
-        System.out.println(" ");
-        System.out.println(HP8020.getTonerLevel());
-        System.out.println(SM6040M.getTonerLevel());
-        System.out.println(" ");
-        System.out.println(HP8020.getPrintedPages());
-        System.out.println(SM6040M.getPrintedPages());
+        Car toyota = new Toyota(8, "Passat");
+        toyota.brake(40);
 
+        Car fiat = new Fiat(4, "Mondeo");
+        fiat.startEngine();
+        fiat.accelerate(100);
+        fiat.brake(60);
+        System.out.println(fiat.getDoors());
+        System.out.println(fiat.getModel());
+        System.out.println(fiat.getCylinders());
+        System.out.println(fiat.isEngine());
     }
 }
---
+++ src/main/java/Toyota.java
@@ -0,0 +1,5 @@
+public class Toyota extends Car{
+    public Toyota(int cylinders, String model) {
+        super(cylinders, model);
+    }
+}
